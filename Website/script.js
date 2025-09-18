ac38d5105c2f9a3311de3769da07b9369215aa37
Psypower999 <169001162+Psypower999@users.noreply.github.com>
2025-09-17 20:37:13 -0700

    info

--- Website/script.js
+++ Website/script.js
@@ -294,10 +294,10 @@ async function displayAlbums() {
     let array = Array.from(anchors)
     for (let index = 0; index < array.length; index++) {
         const e = array[index]; 
-        if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
+        if (e.href.includes("./Website/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let a = await fetch(`/Website/songs/${folder}/info.json`)
+            let a = await fetch(`./Website/songs/${folder}/info.json`)
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
